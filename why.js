f7a90a7c84d74312f9d75a16ae50e4d2fad9a731
Victor <victorgdl89@gmail.com>
2016-11-14 23:44:11 -0600

    add essay

---
+++ why.js
@@ -0,0 +1,22 @@
+
+
+
+confirm("We are about to know reason why you are learning code")
+
+
+var text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    soFar = "This is WHY :           ";
+
+var visible = document.querySelector(".visible"),
+    invisible = document.querySelector(".invisible");
+
+invisible.innerHTML = text;
+var t = setInterval(function(){
+  soFar += text.substr(0, 1),
+  text = text.substr(1);
+
+  visible.innerHTML = soFar;
+  invisible.innerHTML = text;
+
+  if (text.length === 0) clearInterval(t);
+}, 100)
