12e21dd4767f7bdaec79feb45bf7c9eb6853b7a3
Victor <victorgdl89@gmail.com>
2016-11-23 17:59:50 -0600

    code len update

--- why.js
+++ why.js
@@ -4,7 +4,7 @@
 confirm("We are about to know reason why you are learning code")
 
 
-var text = "The reasons why I’m learning how to code are to have a tangible expression of my will.  Other reasons crossed my mind such as: money, fame and even power however,  being able to design, create, and innovate websites, Apps among other things using computer code in my own way would be my real “why” of learning how to code.The other reason that I’m learning how to code is because I would like to compose my own music using code  and I would like to show my performance to the world so they can understand that code can and will be used for many different purposes such as creating music. In that way the music melody would be my software and the computer would be my instrument.The last reason would be…. So I can be remembered after I die. The code that I wrote or composed would be in the World Wide Web available for people to read and use. Lastly, I would like to help other people..",
+var text = "The reasons why I’m learning how to code are to have a tangible expression of my will.  Other reasons crossed my mind such as: money and power however,  being able to design, create, and innovate websites, Apps among other things using computer code in my own way would be my real “why” of learning how to code beacuse that's been my dream for a long time.The other reason that I’m learning how to code is because I would like to compose my own music using code  and I would like to show my performance to the world so they can understand that code can and will be used for many different purposes such as creating music. In that way the music melody would be my software and the computer would be my instrument.The last reason would be…. So I can be remembered after I die. The code that I wrote or composed would be in the World Wide Web available for people to read and use. Lastly, I would like to help other people..",
     soFar = "This is WHY :           ";
 
 var visible = document.querySelector(".visible"),
@@ -19,5 +19,9 @@ var t = setInterval(function(){
   visible.innerHTML = soFar;
   invisible.innerHTML = text;
 
-  if (text.length === 0) clearInterval(t);
+  text.indexOf("e");
+  document.getElementById("demo").innerHTML = text;
+
 }, 100)
+
+console.log("your essay length is   " + text.length);
